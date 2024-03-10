1375f47c58958755fb346378c699ea2133555ef7
w3bFinger <mishka321123@gmail.com>
2024-03-10 22:38:52 +0200

    made a registration form, added a function to navigate between login and registration layouts and added a placeholder icon for launching and static icon

--- app/src/main/java/com/meowconnect/SignUpActivity.java
+++ app/src/main/java/com/meowconnect/SignUpActivity.java
@@ -1,4 +1,51 @@
 package com.meowconnect;
 
-public class SignUpActivity {
+import android.graphics.Color;
+import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.TextPaint;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_up_layout);
+        TextView signInPrompt = findViewById(R.id.signInPrompt);
+        String text = "Already have an account? Sign In";
+        SpannableString spannableString = new SpannableString(text);
+
+        ClickableSpan clickableSpan = new ClickableSpan() {
+            @Override
+            public void onClick(@NonNull View widget) {
+
+                finish();
+            }
+
+            @Override
+            public void updateDrawState(TextPaint ds) {
+                super.updateDrawState(ds);
+                ds.setUnderlineText(false);
+                ds.setColor(Color.BLUE);
+            }
+        };
+
+        int signInStart = text.indexOf("Sign In");
+        int signInEnd = signInStart + "Sign In".length();
+        spannableString.setSpan(clickableSpan, signInStart, signInEnd, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        signInPrompt.setMovementMethod(LinkMovementMethod.getInstance());
+        signInPrompt.setText(spannableString, TextView.BufferType.SPANNABLE);
+    }
 }
+
+
